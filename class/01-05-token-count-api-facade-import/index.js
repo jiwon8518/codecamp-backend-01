ead10988d5d30dbe30aa4ff15a2ae3005a4f4623
Jiwon <jiwon8518@naver.com>
2022-01-11 01:24:25 +0900

    day01_0110

---
+++ class/01-05-token-count-api-facade-import/index.js
@@ -0,0 +1,23 @@
+// 다른 js파일의 함수 불러오기
+import { checkValidationPhone, getToken, sendTokenToSMS } from './phone.js';
+
+console.log("안녕하세요~");
+
+// API 만들기
+function createTokenOfPhone(myphone) {
+    
+    // 1. 휴대폰번호 자릿수 맞는지 확인하기
+    const isValid = checkValidationPhone(myphone);
+
+    if(isValid === true) { //if(isValid) { true 생략 가능
+        // 2. 휴대폰 토큰 6자기 만들기
+        const mytoken =  getToken(4);
+    
+        // 3. 핸드폰 번호에 토큰 전송하기
+        sendTokenToSMS(myphone, mytoken);
+    }
+
+}
+
+// API 실행하기
+createTokenOfPhone("01012345678");
