406dca487a4cee89165fe6e685d454be2bf6aeb0
Jiwon <jiwon8518@naver.com>
2022-01-13 19:33:59 +0900

    day04_0113

---
+++ class/04-04-rest-apiwith-express-swagger/index.js
@@ -0,0 +1,67 @@
+import express from 'express';
+import { checkValidationPhone, getToken, sendTokenToSMS } from './phone.js';
+// import * as aaa from './phone.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import { options } from './swagger/config.js';
+
+
+
+//const express = require('express');
+// const swaggerDocument = require('./swagger.json');
+// const swaggerSpec = swaggerJSDoc(options);
+
+
+
+
+
+const app = express();
+app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(options)));
+
+
+
+
+
+app.get('/boards', function (req, res) {
+// 데이터를 조회하는 로직 => DB 에서 꺼내옴
+
+  // res.send('조회에 성공하였습니다')
+  res.send([
+    { number: 1, writer: '철수', title: '제목입니다', contents: "내용입니다"},
+    { number: 2, writer: '영희', title: '제목입니다!', contents: "내용입니다!"},
+    { number: 3, writer: '훈이', title: '제목입니다~', contents: "내용입니다~"},
+  ])
+})
+
+
+
+
+app.post('/boards', function (req, res) {
+// 데이터를 등록하는 로직 => DB 에다 저장함
+
+  console.log(req.body);
+
+  res.send('등록에 성공하였습니다.');
+})
+
+
+
+app.post('/tokens/phone', function (req, res) {
+
+  const myphone = req.body.myphone;
+
+  // 1. 휴대폰번호 자릿수 맞는지 확인하기
+  const isValid = checkValidationPhone(myphone);
+
+  if(isValid === true) { //if(isValid) { true 생략 가능
+      // 2. 휴대폰 토큰 6자기 만들기
+      const mytoken =  getToken(4);
+  
+      // 3. 핸드폰 번호에 토큰 전송하기
+      sendTokenToSMS(myphone, mytoken);
+      res.send('인증완료!!');
+  }
+})
+
+app.listen(3001)
---
+++ class/04-04-rest-apiwith-express-swagger/swagger/borads.swagger.js
@@ -0,0 +1,43 @@
+/**
+ * @openapi
+ * /boards:
+ *   get:
+ *      summary: 게시글 가져오기
+ *      tags: [Board]
+ *      parameters:
+ *          - in: query
+ *            name: number
+ *            type: int
+ *              
+ *      responses:
+ *          200:
+ *              description: 성공
+ *              content:
+ *                  application/json:
+ *                      schema: 
+ *                        type: object
+ *                        properties:
+ *                           number:
+ *                              type: int
+ *                              example: 1
+ *                           writer:
+ *                              type: string
+ *                              example: 철수
+ *                           title:
+ *                              type: string
+ *                              example: 제목입니다
+ *                           contents:
+ *                              type: string
+ *                              example: 내용이에용
+ *                        
+ */
+
+/**
+ * @openapi
+ * /:
+ *   post:
+ *     description: Welcome to swagger-jsdoc!
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
