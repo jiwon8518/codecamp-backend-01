c31a911ef9c7552edce8d166afcd5d6a85b2e4cb
Jiwon <jiwon8518@naver.com>
2022-01-14 11:05:14 +0900

    day04_0113

--- class/04-04-rest-apiwith-express-swagger/swagger/borads.swagger.js
+++ class/04-04-rest-apiwith-express-swagger/swagger/borads.swagger.js
@@ -31,13 +31,3 @@
  *                              example: 내용이에용
  *                        
  */
-
-/**
- * @openapi
- * /:
- *   post:
- *     description: Welcome to swagger-jsdoc!
- *     responses:
- *       200:
- *         description: Returns a mysterious string.
- */
