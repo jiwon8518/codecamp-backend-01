f76292852a59121f6aac60a7f17fc5f1a5aeec0e
Jiwon <jiwon8518@naver.com>
2022-01-27 13:53:56 +0900

    알고리즘 14일차

---
+++ algorithm/day14/02_additionMatrix.js
@@ -0,0 +1,87 @@
+//-- 행렬의 덧셈 --//
+
+// 문제 설명
+// 행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과가 됩니다.
+// 2개의 행렬 arr1과 arr2를 입력받아, 행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요.
+
+// 제한 조건
+// - 행렬 arr1, arr2의 행과 열의 길이는 500을 넘지 않습니다.
+
+// 입출력 예
+// arr1             arr2	            return
+// [[1,2],[2,3]]	[[3,4],[5,6]]	    [[4,6],[7,9]]
+// [[1],[2]]	    [[3],[4]]	        [[4],[6]]
+
+function solution(arr1, arr2) {
+  let answer = [];
+
+  // 1.arr1 배열의 전체 배열들을 가져오고
+  for (let i = 0; i < arr1.length; i++) {
+    let tmp = [];
+    // 2. arr1 배열에서 가져온 배열들의 안쪽 데이터를 조회
+    for (let j = 0; j < arr1[i].length; j++) {
+      tmp.push(arr1[i][j] + arr2[i][j]);
+    }
+
+    answer.push(tmp);
+  }
+  //   console.log("answer: " + answer);
+  return answer;
+}
+
+let arr1 = [
+  [1, 2],
+  [2, 3],
+];
+let arr2 = [
+  [3, 4],
+  [5, 6],
+];
+
+solution(arr1, arr2);
+
+// 멘토님 풀이
+// map() 사용
+function solution(arr1, arr2) {
+  const answer = arr1.map((num1, i) => {
+    console.log(num1, arr2[i]);
+    return num1.map((num2, l) => {
+      console.log(num2, arr2[i][l]);
+      return num2 + arr2[i][l];
+    });
+  });
+  console.log(answer);
+  return answer;
+}
+
+// for문
+function solution(arr1, arr2) {
+  const answer = [[]];
+
+  // 1. arr1 배열의 전체 배열들을 가져오고
+  for (let i = 0; i < arr1.length; i++) {
+    // 2. arr1 배열에서 가져온 배열들의 안쪽 데이터를 조회
+    for (let l = 0; l < arr1[i].length; l++) {
+      // 3. i와 l 인덱스로 값으로 각각의 위치에 해당되는 데이터들을 합
+      const sum = arr1[i][l] + arr2[i][l];
+      // 4. i에 해당되는 인덱스로 접근했을 때 배열이 없을 때는 빈 배열을 생성
+      if (answer[i] === undefined) {
+        answer[i] = [];
+      }
+      // 5. i와 l 인덱스 값으로 해당 위치에 데이터를 삽입
+      answer[i][l] = sum;
+    }
+  }
+  return answer;
+}
+
+let arr1 = [
+  [1, 2],
+  [2, 3],
+];
+let arr2 = [
+  [3, 4],
+  [5, 6],
+];
+
+solution(arr1, arr2);
