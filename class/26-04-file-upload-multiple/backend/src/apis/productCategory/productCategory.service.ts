8605c36ccb8536a3cb8131c6c5dc5c7b6ee97331
Jiwon <jiwon8518@naver.com>
2022-02-22 19:10:07 +0900

    vmInstance Test

---
+++ class/26-04-file-upload-multiple/backend/src/apis/productCategory/productCategory.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ProductCategory } from './entities/productCategory.entity';
+
+@Injectable()
+export class ProductCategoryService {
+  constructor(
+    @InjectRepository(ProductCategory)
+    private readonly productCategoryRepository: Repository<ProductCategory>,
+  ) {}
+  async create({ name }) {
+    // 카테고리를 데이터베이스에 저장
+    await this.productCategoryRepository.save({ name }); // save 등록하고 가지고 오기, create 등록하기, name: name 생략가능
+  }
+
+  async delete({ productCategoryId }) {
+    return await this.productCategoryRepository.delete({
+      id: productCategoryId,
+    });
+  }
+}
